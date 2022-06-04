798ffc8255747136e34f10de06b8026af43af8d0
zack <106865647+crackedzack@users.noreply.github.com>
2022-06-04 13:00:45 +0000

    Initial commit

---
+++ myname.js
@@ -0,0 +1,8 @@
+
+
+// A project in which we have to create a variable and then display it using message.log.
+
+let myname = Zack ; 
+
+message.log (myname) ;
+
---
+++ nodejs-index.js/index.js
@@ -0,0 +1,27 @@
+
+//from https://www.youtube.com/watch?v=IMDApd-aRoc
+
+let username = "zack";
+
+// defined a variable username using let, and then after = sign in quotes " " defined its value
+
+console.log (username , "how are you, welcome to microverse , replit" ) ;
+
+// displayed and called defined variable username using console.log, and then after opening a bracket, first called variable username, the in quotes " " defined its value as a normal text/str.
+
+let ttlstudents = 40 ; 
+let ttlpresent  = 30 ; 
+
+// defined two new variable using let, and  after = sign defined their values.
+
+let Absent = ttlstudents - ttlpresent ;
+
+// now defined action to be taken, i.e. define a new variable Named Absent, and after = define in it, the  two new action between old defined varilable numbers and bring its answer into Absent.
+
+"<br>" ;
+"<br>" ;
+  
+console.log  ("total", Absent, "studnets are absent today" )  ;
+
+
+// displayed and called defined variable Absent, using console.log, and then after opening a bracket, first called variable username, the in quotes " " defined its value as a normal text/str.
